05168f4ce328ae8e1750bd18a87add3fe234b008
cutelee <soyoung.dev@gmail.com>
2019-07-11 18:40:22 +0900

    style: 모바일, 웹 헤더 레이아웃 수정

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -4,14 +4,16 @@ import './Header.css';
 
 function Header() {
   return (
-    <div className="app-header">
-      <h5 className="my-0 mr-md-auto font-weight-normal">타임라인</h5>
-      <nav className="my-2 my-md-0 mr-md-3">
-        <Link className="p-2 text-dark" to="/timeline">타임라인</Link>
-        <Link className="p-2 text-dark" to="/friends">친구목록</Link>
-      </nav>
-      <Link className="btn btn-sm btn-outline-primary" to="/login">로그인</Link>
-    </div>
+    <>
+      <div className="app-header">
+        <h5 className="app-title">Time<span className="color-emphasize">line</span></h5>
+        <nav className="my-2 my-md-0 mr-md-3 web-only">
+          <Link className="p-2 text-dark" to="/timeline">타임라인</Link>
+          <Link className="p-2 text-dark" to="/friends">친구목록</Link>
+        </nav>
+        <Link className="btn btn-sm" to="/login">로그인</Link>
+      </div>
+    </>
   );
 }
 
